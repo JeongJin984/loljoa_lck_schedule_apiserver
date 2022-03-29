363beba6e7a37b246c7327080bd5187e65283a5c
jeongjin984 <philipwq3963@gmail.com>
2022-03-29 15:04:02 +0900

    n

---
+++ README.md
@@ -0,0 +1,7 @@
+## 2022.3.29
+
+### 주요 업데이트
+
+- health, prometheus, metrics 엔드 포인트 생성
+- LivenessProbe ReadinessProbe 엔드 포인트 생성
+- Liveness, Readiness 상태 바꾸는 엔드 포인트 생성
--- src/main/java/com/loljoa/server/web/api/healthController.java
+++ src/main/java/com/loljoa/server/web/api/healthController.java
@@ -1,9 +1,49 @@
 package com.loljoa.server.web.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.availability.ApplicationAvailability;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class healthController {
+    private final ApplicationAvailability availability;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Application State\n" +
+                "Liveness: " + availability.getLivenessState() + "\n" +
+                "Readiness" + availability.getReadinessState();
+    }
+
+    @GetMapping("/block")   //Readiness Block
+    public String block() {
+        AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.REFUSING_TRAFFIC);
+        return "Blocked requests";
+    }
+
+    @GetMapping("/turnoff")   //Readiness Block
+    public String turnoff() {
+        AvailabilityChangeEvent.publish(eventPublisher, this, LivenessState.BROKEN);
+        return "broken";
+    }
+
+    @Async
+    @EventListener
+    public void onStateChanged(AvailabilityChangeEvent<ReadinessState> readiness) throws InterruptedException {
+//        System.out.println("State is changed to " + readiness.getState());
+//        if(readiness.getState().equals(ReadinessState.REFUSING_TRAFFIC)) {
+//            Thread.sleep(5000L);
+//            AvailabilityChangeEvent.publish(eventPublisher, this, ReadinessState.ACCEPTING_TRAFFIC);
+//        }
+//        Action When State Changed
+    }
 }
